8ddeff00737f2df80f4b03fa9b9677647aa8f92a
Your Name <dikaris@naver.com>
2025-05-22 11:59:13 +0900

    feat:updated

---
+++ src/pages/results.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function ResultsPage() {
+  return (
+    <>
+      <Head>
+        <title>Test Results</title>
+      </Head>
+      <div>
+        <h1>Results Page</h1>
+        <p>This is the test results page.</p>
+      </div>
+    </>
+  );
+}
+
+export default ResultsPage;
---
+++ src/pages/test.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function TestPage() {
+  return (
+    <>
+      <Head>
+        <title>Test Page</title>
+      </Head>
+      <div>
+        <h1>Test Page</h1>
+        <p>This is the main test page.</p>
+      </div>
+    </>
+  );
+}
+
+export default TestPage;
